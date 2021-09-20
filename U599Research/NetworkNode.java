3cebe3499bf1bf727f6e8a87dcac81eb96282ef2
Tallon <tallon.mcabee@gmail.com>
2021-09-20 10:14:40 -0400

    Graph Generator work

--- U599Research/NetworkNode.java
+++ U599Research/NetworkNode.java
@@ -1,6 +1,73 @@
 package U599Research;
 
+import java.util.*;
+
 public class NetworkNode 
 {
+	private ArrayList node;
+	private int nodeNumber;
+	private int arraySize;
+	
+	public NetworkNode(int i)
+	{
+		node = new ArrayList<>();
+		nodeNumber = i;
+		arraySize = 0;
+	}
+	
+	public Boolean addEdge(NetworkNode n)
+	{
+		boolean alreadyAdded = false;
+		for (int i = 0; i < arraySize; i++)
+		{
+			if (n == node.get(i))
+			{
+				alreadyAdded = true;
+			}
+		}
+		
+		if (alreadyAdded)
+		{
+			System.out.println("Connection already exists");
+		}
+		else
+		{
+			this.node.add(n);
+			arraySize += 1;
+		}
+		
+		return alreadyAdded;
+	}
 
+	public ArrayList getNode() 
+	{
+		return node;
+	}
+
+	public void setNode(ArrayList node) 
+	{
+		this.node = node;
+	}
+
+	public int getNodeNumber() 
+	{
+		return nodeNumber;
+	}
+
+	public void setNodeNumber(int nodeNumber) 
+	{
+		this.nodeNumber = nodeNumber;
+	}
+
+	public int getArraySize() 
+	{
+		return arraySize;
+	}
+
+	public void setArraySize(int arraySize) 
+	{
+		this.arraySize = arraySize;
+	}
+	
+	
 }
--- U599Research/graphCreator.java
+++ U599Research/graphCreator.java
@@ -1,18 +1,46 @@
 package U599Research;
 
+import java.util.*;
+import java.io.*;
+
 public class graphCreator 
 {
 	private int numberOfNodes = 0;
+	private int numberOfEdges = 0;
 	
+	@SuppressWarnings("unchecked")
 	public graphCreator(int n)
 	{
 		//constructor that creates a random graph with random edge weights based upon the arguement feed into the method
-		//may need to have seperate class
+		//may need to have separate class
+		numberOfNodes = n;
+		
+		Random r = new Random();
+		
+		numberOfEdges = r.nextInt(numberOfNodes) + 1;
+		
+		List<List<NetworkNode>> graphList = new ArrayList<>(numberOfNodes);
+		
+		for (int i = 0; i < numberOfNodes; i++)
+		{
+			NetworkNode node = new NetworkNode(i);
+			graphList.add((List<NetworkNode>) node);
+		}
+		
+		for (int i = 0; i < numberOfEdges; i++)
+		{
+			for (int b = 0; b < numberOfNodes; b++)
+			{
+				
+			}
+		}
+		
 	}
 	
-	public void addEdge(int n)
+	public void addEdge(NetworkNode a, NetworkNode b)
 	{
-		
+		a.addEdge(b);
+		b.addEdge(a);
 	}
 	
 }
