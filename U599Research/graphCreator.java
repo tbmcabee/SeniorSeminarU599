abcd58450d0d8cd7888b44fec1a82f5813756299
Tallon <77416208+tbmcabee@users.noreply.github.com>
2021-09-19 22:58:12 -0400

    Add files via upload

---
+++ U599Research/graphCreator.java
@@ -0,0 +1,18 @@
+package U599Research;
+
+public class graphCreator 
+{
+	private int numberOfNodes = 0;
+	
+	public graphCreator(int n)
+	{
+		//constructor that creates a random graph with random edge weights based upon the arguement feed into the method
+		//may need to have seperate class
+	}
+	
+	public void addEdge(int n)
+	{
+		
+	}
+	
+}
